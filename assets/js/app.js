43707abef12f00f76bf113d6f3d09ec995da2c3a
Jacob124 <jbelanger716@gmail.com>
2018-12-09 11:44:23 -0500

    test5

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ const {remote} = require('electron')
 const { Menu, MenuItem } = remote
 const fs = require('fs')
 
+console.log("Version 3")
 //************************************************************** */
 // Require Custom Modules
 //************************************************************** */
